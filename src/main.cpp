8627081763ad262904d25eaf265930b590984a6e
Jerrywu4023 <jerrywu4023@gmail.com>
2024-11-19 20:56:20 -0500

    Runs auto in driver

--- src/main.cpp
+++ src/main.cpp
@@ -15,10 +15,15 @@ void autonomous() {
 
 void opcontrol() {
 	bool program = true;
+	int autoButton;
 	
 	while (program) {
 		
 		tankDrive();
+
+		autoButton = master.get_digital(pros::E_CONTROLLER_DIGITAL_DOWN);
+		if (autoButton) 
+			test();
 
 		pros::delay(10);
 	}
