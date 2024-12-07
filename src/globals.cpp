5ef4c707b9ffd75b55c8e6e7b4f8d209608a22c0
colonelzac <artemis.fleming115@gmail.com>
2024-12-06 20:02:52 -0800

    Update globals.cpp

--- src/globals.cpp
+++ src/globals.cpp
@@ -6,24 +6,24 @@
 pros::Controller master(pros::E_CONTROLLER_MASTER);
 
 // Drive Motors (ordered from front to back)
-pros::Motor L1 (15, pros::E_MOTOR_GEARSET_06, 0, pros::E_MOTOR_ENCODER_DEGREES);
-pros::Motor L2 (16, pros::E_MOTOR_GEARSET_06, 1, pros::E_MOTOR_ENCODER_DEGREES);
-pros::Motor L3 (17, pros::E_MOTOR_GEARSET_06, 0, pros::E_MOTOR_ENCODER_DEGREES);
-pros::Motor L4 (18, pros::E_MOTOR_GEARSET_06, 1, pros::E_MOTOR_ENCODER_DEGREES);
-pros::Motor L5 (3, pros::E_MOTOR_GEARSET_06, 0, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor L1 (20, pros::E_MOTOR_GEARSET_06, 0, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor L2 (19, pros::E_MOTOR_GEARSET_06, 1, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor L3 (18, pros::E_MOTOR_GEARSET_06, 0, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor L4 (17, pros::E_MOTOR_GEARSET_06, 1, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor L5 (16, pros::E_MOTOR_GEARSET_06, 0, pros::E_MOTOR_ENCODER_DEGREES);
 //motor #, port #, gear ratio, fwd/rvrs, encoder units
 pros::Motor R1 (11, pros::E_MOTOR_GEARSET_06, 1, pros::E_MOTOR_ENCODER_DEGREES);
 pros::Motor R2 (12, pros::E_MOTOR_GEARSET_06, 0, pros::E_MOTOR_ENCODER_DEGREES);
 pros::Motor R3 (13, pros::E_MOTOR_GEARSET_06, 1, pros::E_MOTOR_ENCODER_DEGREES);
 pros::Motor R4 (14, pros::E_MOTOR_GEARSET_06, 0, pros::E_MOTOR_ENCODER_DEGREES);
-pros::Motor R5 (4, pros::E_MOTOR_GEARSET_06, 1, pros::E_MOTOR_ENCODER_DEGREES);
+pros::Motor R5 (15, pros::E_MOTOR_GEARSET_06, 1, pros::E_MOTOR_ENCODER_DEGREES);
 // red - 36:1, green - 18:1 blue - 6:1
 
 // Pneumatics
 pros::ADIDigitalOut PTO ('A');
 
 // Sensors
-pros::IMU imu1 (19);
-pros::Rotation trackingL (2);
+pros::IMU imu1 (10);
+pros::Rotation trackingL (1);
 
 double imu1Multi = 1;
